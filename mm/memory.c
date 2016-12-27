318c7605718ee0798b028eeb2c7241529141ccfe
Mon@diac <khanote@outlook.com>
2016-12-27 15:54:50 +0100

    Build v1.4
    Change-Id: I31f514f1e2e337f237bcd36c7811f0692c88dbe9

--- mm/memory.c
+++ mm/memory.c
@@ -1488,24 +1488,6 @@ static void unmap_single_vma(struct mmu_gather *tlb,
 	}
 }
 
-static inline bool can_follow_write_pte(pte_t pte, struct page *page,
-					unsigned int flags)
-{
-	if (pte_write(pte))
-		return true;
-
-	/*
-	 * Make sure that we are really following CoWed page. We do not really
-	 * have to care about exclusiveness of the page because we only want
-	 * to ensure that once COWed page hasn't disappeared in the meantime
-	 * or it hasn't been merged to a KSM page.
-	 */
-	if ((flags & FOLL_FORCE) && (flags & FOLL_COW))
-		return page && PageAnon(page) && !PageKsm(page);
-
-	return false;
-}
-
 /**
  * unmap_vmas - unmap a range of memory covered by a list of vma's
  * @tlb: address of the caller's struct mmu_gather
@@ -1823,7 +1805,7 @@ split_fallthrough:
 			unlock_page(page);
 		}
 	}
-
+unlock:
 	pte_unmap_unlock(ptep, ptl);
 out:
 	return page;
